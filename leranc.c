e9197e397abaceb1f82e9ea8fa1b554d77a9530e
emin690 <mehmeteminaydogdu498@gmail.com>
2025-09-23 20:42:52 +0300

    Add files via upload

---
+++ leranc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int x, y, z;
+    printf("Enter three numbers:\n");
+    scanf("%d %d %d", &x, &y, &z);
+
+    float average = (x + y + z) / 3.0;  // 3.0 ile float bölme
+    printf("Average: %.2f\n", average); // 2 basamaklı gösterim
+
+    return 0;
+}
